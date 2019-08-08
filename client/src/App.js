560f50cb89dec1ec1a1e55e0d337688b392d486c
kckartik17 <kckartik17@gmail.com>
2019-08-08 16:18:56 +0530

    auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useContext} from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthContext from './context/auth/authContext'
+import AuthState from './context/auth/AuthState'
+
+const App = () => {
+
+  const authContext = useContext(AuthContext)
+
+  useEffect(() => {
+    authContext.loadUser();
+  },[])
+
+  render() {
+    return (
+      <AuthState>
+        <div className='App'>
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </AuthState>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,148 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import AuthContext from "./authContext";
+import AuthReducer from "./authReducer";
+
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from "../../types";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+  };
+
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  // Load user
+
+  const loadUser = () => getState => {
+    // User loading
+    dispatch({ type: USER_LOADING });
+
+    axios
+      .get("/api/auth/user", tokenConfig(getState))
+      .then(res =>
+        dispatch({
+          type: USER_LOADED,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({
+          type: AUTH_ERROR
+        });
+      });
+  };
+
+  const register = ({ name, email, password }) => {
+    // Headers
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    // Request body
+    const body = JSON.stringify({ name, email, password });
+
+    axios
+      .post("/api/users", body, config)
+      .then(res =>
+        dispatch({
+          type: REGISTER_SUCCESS,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch(
+          returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+        );
+        dispatch({
+          type: REGISTER_FAIL
+        });
+      });
+  };
+
+  // Login User
+  const login = ({ email, password }) => {
+    // Headers
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    // Request body
+    const body = JSON.stringify({ email, password });
+
+    axios
+      .post("/api/auth", body, config)
+      .then(res =>
+        dispatch({
+          type: LOGIN_SUCCESS,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch(
+          returnErrors(err.response.data, err.response.status, "LOGIN_FAIL")
+        );
+        dispatch({
+          type: LOGIN_FAIL
+        });
+      });
+  };
+
+  const logout = () => dispatch({ type: LOGOUT_SUCCESS });
+
+  const tokenConfig = getState => {
+    // Get token from localstorage
+    const token = getState().auth.token;
+
+    // Headers
+    const config = {
+      headers: {
+        "Content-type": "application/json"
+      }
+    };
+
+    // If token, add to headers
+    if (token) {
+      config.headers["x-auth-token"] = token;
+    }
+
+    return config;
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        isLoading: state.isLoading,
+        user: state.user,
+        loadUser,
+        register,
+        login,
+        logout,
+        tokenConfig
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
